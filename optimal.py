8046050b96876d7a66d0bd6ad50a2bed55b97f82
mnbvmnbv2 <ferdinand@ytteborg.com>
2024-06-12 15:03:25 +0200

    rewrite resource

--- optimal.py
+++ optimal.py
@@ -2,35 +2,67 @@ import numpy as np
 import time
 
 
+class Resource:
+    def __init__(self, price, cost_mult, cost_increase_step, income) -> None:
+        self.quantity = 0
+        # price data
+        self._price = price
+        self._cost_mult = cost_mult
+        self._cost_increase_step = cost_increase_step
+        #
+        self._one_income = income
+
+    def buy_one(self):
+        self.quantity += 1
+        self._price = (self._price * self._cost_mult) + self._cost_increase_step
+
+    def get_break_even(self):
+        return self._price / self._one_income
+
+    @property
+    def income(self):
+        return self._one_income * self.quantity
+
+    @property
+    def price(self):
+        return self._price
+
+
 class Game:
     def __init__(self) -> None:
         self.income_mult = 1
-        self.COST_INCREASE = np.array([1.07, 1.15, 1.25])  # Multiplier
-        self.INCOMES = np.array([3, 300, 5]) * self.income_mult
-        self.COST_INCREASE_STEP = np.array([1.1, 1.2, 1.3])
+        self.resources = [
+            Resource(price=4, cost_mult=1.07, cost_increase_step=0, income=3),
+            Resource(price=300, cost_mult=1.15, cost_increase_step=0, income=300),
+            Resource(price=6, cost_mult=1.25, cost_increase_step=0, income=5),
+        ]
         self.GOAL = 1000
-
-        self.costs = np.array([4, 300, 6], dtype=float)
-        self.owned = np.array([0, 0, 0])
         self.money = 200.0
         self.step_ = 0
 
     @property
     def income(self):
-        return np.sum(self.owned * self.INCOMES)
+        return sum(r.income for r in self.resources)
+
+    @property
+    def owned(self):
+        return [r.quantity for r in self.resources]
+
+    @property
+    def costs(self):
+        return [r.price for r in self.resources]
 
     def get_values(self):
         # cps = self.INCOMES / self.costs
-        break_even_times = self.costs / self.INCOMES
+        break_even_times = [r.get_break_even() for r in self.resources]
         return break_even_times
 
     def buy(self, idx):
         if idx == -1:
             return False
-        if self.money >= self.costs[idx]:
-            self.owned[idx] += 1
-            self.money -= self.costs[idx]
-            self.costs[idx] = self.costs[idx] * self.COST_INCREASE[idx]
+        if self.money >= self.resources[idx].price:
+            self.money -= self.resources[idx].price
+            self.resources[idx].buy_one()
             return True
         return False
 
